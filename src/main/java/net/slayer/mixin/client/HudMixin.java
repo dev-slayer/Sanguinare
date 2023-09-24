35aac2a55568a566df5e7834a90838cd1391414f
SlayerDev <anthonywebster@outlook.com>
2023-09-24 19:47:01 -0400

    test???

---
+++ src/main/java/net/slayer/mixin/client/HudMixin.java
@@ -0,0 +1,9 @@
+package net.slayer.mixin.client;
+
+import net.minecraft.client.gui.hud.InGameHud;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(InGameHud.class)
+public class HudMixin {
+
+}
