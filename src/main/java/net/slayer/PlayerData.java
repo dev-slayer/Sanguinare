6d2634d266a4d356e789301c76650cef809aed32
Isaac Menchaca <95837389+PainChungles@users.noreply.github.com>
2023-09-24 08:43:58 -0500

    Tried too add a regen multiplier foundation
    currently it doesn't do jackshit, but i tried too make a foundation for it too work once i figure out what the fuck is health and shit

--- src/main/java/net/slayer/PlayerData.java
+++ src/main/java/net/slayer/PlayerData.java
@@ -3,4 +3,5 @@ package net.slayer;
 public class PlayerData {
     boolean sanguinareStatus;
     int blood;
+    float regenMult; // adding regeneration multiplier for my fucking balls n' shit lol -chungles
 }
