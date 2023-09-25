7c38ed7db3d95060e600c1b80bf368a362c490b5
SlayerDev <anthonywebster@outlook.com>
2023-09-24 23:55:19 -0400

    added 1 silly comment

--- src/main/java/net/slayer/effects/ScarletSickness.java
+++ src/main/java/net/slayer/effects/ScarletSickness.java
@@ -39,6 +39,7 @@ public class ScarletSickness extends StatusEffect {
             }
 
             // defines if the entity is a goddamn player or nah -chungles
+                // dawg this isn't even true it just takes the PlayerEntity into a ServerPlayerEntity format.. -slayer
             ServerPlayerEntity player = ((PlayerEntity) entity).getServer().getPlayerManager().getPlayer(entity.getUuid());
 
 
